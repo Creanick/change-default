f736dff14e025267ca458476aad2d561a9363f60
manick <manickware@gmail.com>
2019-09-12 10:51:46 +0530

    add changeFile function

---
+++ bin/changeFile.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const changeCode = require("./changeCode");
+function changeFile(path) {
+  return new Promise((resolve, reject) => {
+    try {
+      const isExist = fs.existsSync(path);
+      if (!isExist) throw new Error("Path not exists");
+      const isFile = fs.statSync(path).isFile();
+      if (!isFile) throw new Error("path is not file");
+      const fileData = fs.readFileSync(path, "utf8");
+      fs.writeFileSync(path, changeCode(fileData));
+      resolve("Success");
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+module.exports = changeFile;
