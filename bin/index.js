c824792adcdb30e12d40cf3aa093c1d468c32732
manick <manickware@gmail.com>
2019-09-12 12:22:26 +0530

    minimal product ready

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,32 @@
 #! /usr/bin/env node
-const fs = require("fs");
 const path = require("path");
-const changeCode = require("./changeCode");
+const changeDefault = require("./changeDefault");
+const parser = require("yargs-parser");
+const options = parser(process.argv.slice(2));
+const dir = typeof options.dir === "string" ? options.dir : "";
+const mainPath = path.join(process.cwd(), dir);
+const pkg = require("../package.json");
+if (options.help) {
+  console.log(`
+    change-default
+
+    Options:
+
+    --dir : starting directory [default:/]
+    --help: for help
+    --version: get current version
+  `);
+  return;
+}
+if (options.version) {
+  console.log(pkg.version);
+  return;
+}
+
+changeDefault(mainPath)
+  .then(result => {
+    console.log("changed successfully");
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
